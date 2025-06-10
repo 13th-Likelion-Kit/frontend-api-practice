9885987d252c586faa78b4bc886a30ade79eca6c
JiYi Kim <20230299@kumoh.ac.kr>
2025-06-10 20:28:00 +0900

    6stydying

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 function App() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await axios.get(
+                "http://119.56.230.161:11111/posts"
+            );
+            setPosts(response.data);
+        };
+        fetchPosts();
+    }, []);
+
     return (
         <div>
-            <div>hi</div>
+            {posts.map((post) => {
+                return (
+                    <Card
+                        key={post.id}
+                        title={post.title}
+                        content={post.content}
+                        date={post.createdAt}
+                    />
+                );
+            })}
         </div>
     );
 }
