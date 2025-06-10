1bbcb59938ea67d8d2e4d9754122a71935fda9ec
uzinkkk <yj051888@kumoh.ac.kr>
2025-06-10 20:22:33 +0900

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-    return (
-        <div>
-            <div>hi</div>
-        </div>
-    );
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get("http://119.56.230.161:11111/posts");
+      setPosts(response.data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <Card
+            key={post.id}
+            title={post.title}
+            content={post.content}
+            date={post.createdAt}
+          />
+        );
+      })}
+
+      <div>hi</div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,36 @@
 // CSS는 개인취향대로 사용 (예: styled-components, CSS 모듈 등)
+import { Trash2 } from "lucide-react";
+import styles from "./card.module.scss";
 
-function Card() {
-    return (
-        <div>
-            <div>여기에 조회된 게시글 컴포넌트를 만들어보세요.</div>
-        </div>
-    );
+function Card({ title, content, id, date, onDelete }) {
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`http://119.56.230.161:11111/posts/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete post");
+      }
+
+      onDelete();
+    } catch (err) {
+      console.error("Failed to delete post", err);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.topBar}>
+        <div>{title}</div>
+        <div>{date}</div>
+      </div>
+      <div className={styles.content}>{content}</div>
+      <div className={styles.buttonContainer}>
+        <button onClick={() => handleDelete()}>
+          <Trash2 width={20} onClick={() => onDelete()} />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
