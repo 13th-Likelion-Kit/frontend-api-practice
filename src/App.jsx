c849229e8244195e4c3db508bfc6461fd3822807
leesu <a91364090@gmail.com>
2025-06-10 20:19:09 +0900

    완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './components/Card';
+import axios from 'axios';
 
 function App() {
+    const [posts, setPosts] = useState([]);
+    useEffect( () => {
+        const fetchPosts = async () => {
+            const response = await axios.get(
+                'http://119.56.230.161:11111/posts')
+    setPosts(response.data);
+    };
+    
+    fetchPosts();},[]);
     return (
         <div>
-            <div>hi</div>
+            {posts.map((post) => {
+                return (
+                    <Card
+                    key={post.id}
+                    id={post.id}
+                    title={post.title}
+                    content={post.content}
+                    date={post.date}/>
+                );
+            })}
         </div>
     );
 }
